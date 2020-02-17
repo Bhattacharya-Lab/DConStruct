a8b81e62ba47290f4cd32f6b715a3cc918471ecd
Roche78 <rahmatullahroche@gmail.com>
2020-02-16 22:09:25 -0600

    Update DConStruct.py

--- DConStruct.py
+++ DConStruct.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python
 
-# iCon3D - Improved Contact-assisted 3D Protein Folding Protocol
-
-# By Rahmatullah Roche & Debswapna Bhattacharya, December 2019
-
-# License: GPL-3.0
-
-# Copyright (C) Bhattacharya Laboratory 2019
-
-# V1.0
+#  DConStruct: Hybridized Distance- and Contact-based Hierarchical Structure Modeling for Folding Soluble and Membrane Proteins
+#
+#  Copyright (C) Bhattacharya Laboratory 2019
+#
+#  DConStruct is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  DConStruct is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with DConStruct.  If not, see <http://www.gnu.org/licenses/>.
+#
+############################################################################
 
 from __future__ import print_function
 import os
